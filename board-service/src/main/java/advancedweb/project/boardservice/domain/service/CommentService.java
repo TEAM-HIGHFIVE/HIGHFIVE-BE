330e529eb7842051f08d1dee52131c9bd091de67
inyong04 <145008900+inyong04@users.noreply.github.com>
2025-05-12 11:03:32 +0900

    Update CommentService.java

--- board-service/src/main/java/advancedweb/project/boardservice/domain/service/CommentService.java
+++ board-service/src/main/java/advancedweb/project/boardservice/domain/service/CommentService.java
@@ -1,10 +1,12 @@
 package advancedweb.project.boardservice.domain.service;
 
-import advancedweb.project.boardservice.application.dto.request.WriteCmtReq;
 import advancedweb.project.boardservice.domain.entity.Comment;
 import advancedweb.project.boardservice.domain.repository.CommentRepository;
+import advancedweb.project.boardservice.application.dto.request.WriteCmtReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +15,13 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     public void write(String postNo, WriteCmtReq request, String userNo) {
-        // 댓글 생성
-        Comment comment = Comment.create(postNo, userNo, request.content());
+        Comment comment = Comment.builder()
+                .postNo(postNo)
+                .userNo(userNo)
+                .content(request.content())
+                .createdAt(LocalDateTime.now())
+                .build();
 
-        // DB 저장
         commentRepository.save(comment);
     }
 }
