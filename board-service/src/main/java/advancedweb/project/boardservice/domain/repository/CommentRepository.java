f1d06b0154df5b0372405cd4670e7830db66f799
inyong04 <145008900+inyong04@users.noreply.github.com>
2025-05-13 10:59:05 +0900

    Update CommentRepository.java

--- board-service/src/main/java/advancedweb/project/boardservice/domain/repository/CommentRepository.java
+++ board-service/src/main/java/advancedweb/project/boardservice/domain/repository/CommentRepository.java
@@ -2,11 +2,9 @@ package advancedweb.project.boardservice.domain.repository;
 
 import advancedweb.project.boardservice.domain.entity.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
     List<Comment> findByPostNo(String postNo);
 }
