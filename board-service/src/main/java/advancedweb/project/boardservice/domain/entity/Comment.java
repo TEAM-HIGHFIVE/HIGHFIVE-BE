74edffe0fc23454f18c93b928da49c6e1c8df585
inyong04 <145008900+inyong04@users.noreply.github.com>
2025-05-13 10:58:34 +0900

    Update Comment.java

--- board-service/src/main/java/advancedweb/project/boardservice/domain/entity/Comment.java
+++ board-service/src/main/java/advancedweb/project/boardservice/domain/entity/Comment.java
@@ -3,29 +3,21 @@ package advancedweb.project.boardservice.domain.entity;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Document(collection = "comments")
 public class Comment {
 
     @Id
     private String commentNo;
 
-    @Field("post_no")
     private String postNo;
-
-    @Field("user_no")
     private String userNo;
-
-    @Field("content")
     private String content;
-
-    @Field("created_at")
     private LocalDateTime createdAt;
 }
